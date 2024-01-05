fe9237930d6d3c09d5c54ce90c22473629e7586d
ilnur <thander66@gmail.com>
2024-01-05 18:36:59 +0700

    fix

--- serverless.py
+++ serverless.py
@@ -4,8 +4,10 @@ import runpod
 import tempfile
 import requests
 import traceback
+import firebase_admin
 from datetime import datetime
 from dotenv import load_dotenv
+from firebase_admin import firestore
 
 # local imports
 from app.api.functions import do_img2vid, text2img, download_image
@@ -13,6 +15,13 @@ from app.api.functions import do_img2vid, text2img, download_image
 load_dotenv()
 
 # initialize firebase app
+SERVICE_CERT = json.loads(os.getenv("SERVICE_CERT"))
+STORAGE_BUCKET = os.getenv("STORAGE_BUCKET")
+cred_obj = firebase_admin.credentials.Certificate(SERVICE_CERT)
+firebase_admin.initialize_app(cred_obj, {"storageBucket": STORAGE_BUCKET})
+db = firestore.client()
+ref = db.collection("videoList").document()
+
 
 def videoRecordDict(userID):
     return {
@@ -33,9 +42,9 @@ def videoRecordDict(userID):
 def handler(job):
     # get job input
     request = job["input"]
-    # video_record = videoRecordDict(request.get("userID", ""))
+    video_record = videoRecordDict(request.get("userID", ""))
     # add data to firebase
-    # ref.set(video_record)
+    ref.set(video_record)
     if not request.get("image_url"):
         image = download_image(text2img(request["prompt"]))
     else:
@@ -44,10 +53,10 @@ def handler(job):
         return {"error": "Failed to download image"}
     try:
         result = do_img2vid(request, image)
-        # ref.update(result)
+        ref.update(result)
         return result
     except Exception as e:
-        # ref.update({"status": "error", "errorMessage": traceback.format_exc()})
+        ref.update({"status": "error", "errorMessage": traceback.format_exc()})
         return {"error": str(e)}
     finally:
         if os.path.exists(image):
